217076095c9cd85ca05b9b55e6ac1c20476a76b0
Trey Mack <tmack10@gmail.com>
2017-07-07 17:37:04 -0400

    C# Implementations

---
+++ csharp/FizzBuzz.CSharp/FizzBuzz.CSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FizzBuzz.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("Fizz Buzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ csharp/FizzBuzz.CSharp/FizzBuzz.FunctionalCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace FizzBuzz.FunctionalCSharp
+{
+    class Program
+    {
+        static string GetLine(int x) =>
+            x % 3 == 0 && x % 5 == 0 ? "Fizz Buzz"
+                : x % 3 == 0 ? "Fizz"
+                : x % 5 == 0 ? "Buzz"
+                : x.ToString();
+
+        static void Main(string[] args)
+        {
+            var lines = Enumerable.Range(1, 100)
+                .Select(GetLine);
+            foreach (var line in lines)
+                Console.WriteLine(line);
+        }
+    }
+}
